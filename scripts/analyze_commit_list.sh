83aaa09c15d8f8035537937a345d252acd422a71
pad <yoann.padioleau@gmail.com>
2020-10-17 15:46:29 +0200

    Move things around, introduce core/, diff/, rename a few dirs use codegraph to improve the dependencies org

--- scripts/analyze_commit_list.sh
+++ scripts/analyze_commit_list.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-~/github/patchparse/patchparse --git . --min 3 --minf 2 --noall --noev --print-sp --notex --gitcommitlist buglist
+~/github/patchparse/_build/default/cli/main.exe --git . --min 3 --minf 2 --noall --noev --print-sp --notex --gitcommitlist buglist
 

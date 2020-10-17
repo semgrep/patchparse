8ed95b58f28c76df937ac5d28cdb6cd1d7a6a5e1
pad <yoann.padioleau@gmail.com>
2020-10-17 15:08:58 +0200

    * parse_input/cparser2.ml: auto generated

--- scripts/analyze_commit_list.sh
+++ scripts/analyze_commit_list.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-~/github/patchparse/patchparse.opt --git . --min 3 --minf 2 --noall --noev --print-sp --notex --gitcommitlist buglist
+~/github/patchparse/patchparse --git . --min 3 --minf 2 --noall --noev --print-sp --notex --gitcommitlist buglist
 

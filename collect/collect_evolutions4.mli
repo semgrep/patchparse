cd321b8564ae3909335365c00123ffe2c5c114fc
pad <yoann.padioleau@gmail.com>
2020-10-18 10:35:09 +0200

    * collect/collect_evolutions4.mli:

--- collect/collect_evolutions4.mli
+++ collect/collect_evolutions4.mli
@@ -1,4 +1,4 @@
 val collect :
-    Eq_classes.change_table ->
-      (Patch.id(*ver*) * string(*dir*) *
-	 string list(*files*) * Ce.ce list * float) list
+  Eq_classes.change_table ->
+  (Patch.id(*ver*) * Paths.dir(*dir*) * Paths.file list(*files*) * 
+  Ce.ce list * float) list

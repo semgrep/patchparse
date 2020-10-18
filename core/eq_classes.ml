9522521edb1d3dad25c2ba5fbe59fd0f73c97154
pad <yoann.padioleau@gmail.com>
2020-10-18 15:53:41 +0200

    misc

--- core/eq_classes.ml
+++ core/eq_classes.ml
@@ -2,12 +2,12 @@
 type change_table =
     (Ce.ce,
      ((Patch.id (*version*) * Paths.dir (*dir*)) * int (*sites*) *
-	Paths.file list (*files*) * Patch.region list (*regions*) *
-	Ce.ce list (* same as key but without abstraction lines*)) list
+    Paths.file list (*files*) * Patch.region list (*regions*) *
+    Ce.ce list (* same as key but without abstraction lines*)) list
     ) Hashtbl.t
 
 (* the key is the size of the diff.ce of the context_change *)
 type worklist = 
     (int, (Context_change.t * Patch.id (*version*) * Paths.dir (*dir*) *
-	     Paths.file (*file*) * Patch.region (*region*)) list ref)
+         Paths.file (*file*) * Patch.region (*region*)) list ref)
       Hashtbl.t
--- git/git_reader.ml
+++ git/git_reader.ml
@@ -1,41 +1,47 @@
 module Config = Globals
 
+(* conventions: in_lines = input_lines *)
+
+(*****************************************************************************)
+(* Helpers *)
+(*****************************************************************************)
+
 let lines = ref 0
 
 let rec drop_git_start in_lines =
   let rec loop ver code = function
       [] -> (ver,code,[])
     | (n,l)::rest ->
-	match Str.split_delim (Str.regexp " ") l with
-	  "commit"::_ -> drop_git_start ((n,l)::rest)
-	| "diff"::_ -> (ver,code,((n,l)::rest))
-	| "---"::_ -> (ver,code,((n,l)::rest))
-	| _ -> loop ver code rest in
+    match Str.split_delim (Str.regexp " ") l with
+      "commit"::_ -> drop_git_start ((n,l)::rest)
+    | "diff"::_ -> (ver,code,((n,l)::rest))
+    | "---"::_ -> (ver,code,((n,l)::rest))
+    | _ -> loop ver code rest in
   match in_lines with
     [] -> failwith "should not happen"
   | (nc,lc)::(na,la)::(nd,ld)::rest ->
       (match Str.split_delim (Str.regexp " ") lc with
         ["commit";code] ->
-	  (match
+      (match
             Str.split (Str.regexp " ")
               (List.hd (Str.split (Str.regexp "<") la)) with
             "Author:"::name ->
-	      let name = String.concat " " name in
-	      (match Str.split (Str.regexp " ") ld with
-		"Date:"::_::_::_::month::day::_::year::_ ->
-		  let date = String.concat " " [month;day;year] in
-		  let ver = Printf.sprintf "%s %d %s %s" code nc name date in
-		  loop ver code rest
-	      | _ ->
-		  failwith
-		    (Printf.sprintf "%s: date: bad git file %s" code ld))
-	  | "Merge:"::_ -> loop "" "" rest
-	  | _ ->
-	      failwith
-		(Printf.sprintf "%s: after commit: bad git file %s" code la))
+          let name = String.concat " " name in
+          (match Str.split (Str.regexp " ") ld with
+        "Date:"::_::_::_::month::day::_::year::_ ->
+          let date = String.concat " " [month;day;year] in
+          let ver = Printf.sprintf "%s %d %s %s" code nc name date in
+          loop ver code rest
+          | _ ->
+          failwith
+            (Printf.sprintf "%s: date: bad git file %s" code ld))
+      | "Merge:"::_ -> loop "" "" rest
+      | _ ->
+          failwith
+        (Printf.sprintf "%s: after commit: bad git file %s" code la))
       | _ -> drop_git_start ((na,la)::(nd,ld)::rest))
   | _ -> failwith "bad git file"
-	    
+        
 let drop_patch_start in_lines =
   ("no_ver","no_code",in_lines)
 
@@ -44,10 +50,10 @@ let get_diffs in_lines =
       [] -> (List.rev acc,[])
     | (n,"")::rest -> loop acc rest
     | (n,l)::rest ->
-	match Str.split (Str.regexp " ") l with
-	  ["commit";code] -> (List.rev acc,(n,l)::rest)
-	| _ ->
-	    loop ((n,l) :: acc) rest in
+    match Str.split (Str.regexp " ") l with
+      ["commit";code] -> (List.rev acc,(n,l)::rest)
+    | _ ->
+        loop ((n,l) :: acc) rest in
   loop [] in_lines
 
 let counter l =
@@ -56,34 +62,38 @@ let counter l =
     | x::xs -> loop ((n,x) :: acc) (n+1) xs in
   loop [] 1 l
 
+(*****************************************************************************)
+(* Entry points *)
+(*****************************************************************************)
+
 let git fl =
   lines := 0;
   let in_lines =
-    if !Config.gitpatch
-    then Aux.cmd_to_list (Printf.sprintf "/bin/cat %s" fl)
-    else if !Config.gitcommitlist
-    then
+    match () with
+    | _ when !Config.gitpatch ->
+        Aux.cmd_to_list (Printf.sprintf "/bin/cat %s" fl)
+    | _ when !Config.gitcommitlist ->
       let commits = Aux.cmd_to_list (Printf.sprintf "/bin/cat %s" fl) in
-      List.fold_left
-	(fun prev commit ->
-	  (Aux.cmd_to_list
-	    (Printf.sprintf "cd %s ; /usr/bin/git show %s"
-	       !Config.gitdir commit)) @ prev)
-	[] commits
-    else
+      List.fold_left (fun prev commit ->
+        (Aux.cmd_to_list
+          (Printf.sprintf "cd %s ; /usr/bin/git show %s"
+           !Config.gitdir commit)) @ prev
+        )
+        [] commits
+    | _ -> 
       Aux.cmd_to_list
-	(Printf.sprintf
-	   "cd %s ; /usr/bin/git log -p %s %s"
-	   !Config.gitdir fl !Config.git_restrict) in
+       (Printf.sprintf "cd %s ; /usr/bin/git log -p %s %s"
+            !Config.gitdir fl !Config.git_restrict) 
+  in
   let in_lines = counter in_lines in
   let rec loop acc in_lines ctr =
     match in_lines with
     | [] -> List.rev acc
     | in_lines ->
-	let (info,code,in_lines) = drop_git_start in_lines in
-	let (lines,in_lines) = get_diffs in_lines in
-	Hashtbl.add Config.version_table ctr (Printf.sprintf "%s" info);
-	loop ((Patch.Id ctr,lines) :: acc) in_lines (ctr + 1) in
+    let (info,code,in_lines) = drop_git_start in_lines in
+    let (lines,in_lines) = get_diffs in_lines in
+    Hashtbl.add Config.version_table ctr (Printf.sprintf "%s" info);
+    loop ((Patch.Id ctr,lines) :: acc) in_lines (ctr + 1) in
   loop [] in_lines 0
 
 let patch fl =
@@ -94,8 +104,8 @@ let patch fl =
     match in_lines with
       [] -> []
     | in_lines ->
-	let (info,code,in_lines) = drop_patch_start in_lines in
-	let (lines,in_lines) = get_diffs in_lines in
-	Hashtbl.add Config.version_table ctr (Printf.sprintf "%s" info);
-	(Patch.Id ctr,lines) :: loop in_lines (ctr + 1) in
+    let (info,code,in_lines) = drop_patch_start in_lines in
+    let (lines,in_lines) = get_diffs in_lines in
+    Hashtbl.add Config.version_table ctr (Printf.sprintf "%s" info);
+    (Patch.Id ctr,lines) :: loop in_lines (ctr + 1) in
   loop in_lines 0

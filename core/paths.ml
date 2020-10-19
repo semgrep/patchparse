2491bdc196023d855ccb3e5e192937c77633a7e2
pad <yoann.padioleau@gmail.com>
2020-10-19 16:31:33 +0200

    * core/paths.ml: comment

--- core/paths.ml
+++ core/paths.ml
@@ -2,6 +2,10 @@
 (* can't be called path.ml because of possible linking conflict with 
  * compiler-libs/ when also use pfff libs and ppx *)
 
+(* This used to be simple string aliases, but I've introduced a constructor
+ * (a la Haskell newtype) to correctly identify all strings that 
+ * were dirs or files.
+ *)
 type dir = Dir of string
  [@@deriving show { with_path = false }]
 type file = File of string

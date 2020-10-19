c35eaa7ef19257cf2658d840549274b0d8c1ce44
pad <yoann.padioleau@gmail.com>
2020-10-19 16:48:02 +0200

    * parsing_patch/parse_patch.mli: new file

---
+++ parsing_patch/parse_patch.mli
@@ -0,0 +1,8 @@
+
+(* this used to be in init.ml *)
+val process_file: 
+  (Patch.id * Paths.dir * Paths.file -> 
+   int (* start_line *) -> Patch.region -> 
+  string (* minus part *) -> string (* plus part *) -> 
+  'b list) ->
+  Patch.t -> 'b list

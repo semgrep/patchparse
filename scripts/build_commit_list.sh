fa8d4c168faf3ad4f75f1434e47b53128490e176
pad <yoann.padioleau@gmail.com>
2020-10-21 09:57:02 +0200

    * scripts/build_commit_list.sh: comment

--- scripts/build_commit_list.sh
+++ scripts/build_commit_list.sh
@@ -3,6 +3,6 @@
 mkdir -p out
 rm -f out/*
 
-# --grep=bug 
+# --grep=bug --grep=fix
 git log --all -i --grep=bugfix --grep=critic --grep=secur --grep=cve --pretty='format:%H' > buglist
 wc -l buglist
